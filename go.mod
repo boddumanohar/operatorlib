f6022e318b0b507cb3b02cd035bc99f10a7641e1
Ankit R Gadiya <git@argp.in>
2019-09-19 00:25:13 +0530

    feat: added operations package
    Operations package implements lower-level functions and Objects which
    will be used by other Objects to interact with the cluster.

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gogo/protobuf v1.2.1 // indirect
 	github.com/json-iterator/go v1.1.7 // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/pkg/errors v0.8.1
 	github.com/spf13/pflag v1.0.3 // indirect
 	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
 	gopkg.in/yaml.v2 v2.2.2 // indirect
---
+++ pkg/operation/operation.go
@@ -0,0 +1,133 @@
+package operation
+
+import (
+	"context"
+	"reflect"
+
+	"argp.in/go/operatorlib/pkg/interfaces"
+
+	"github.com/pkg/errors"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+// Create is a generic create function for any Kubernetes Object. This
+// function can create the Object defined in Conf. This function is a
+// lower level function and is supposed to be used by other Kubernetes
+// Objects. However, this can also be used to create Custom Objects
+// (or unsupported objects).
+func Create(c Conf) (reconcile.Result, error) {
+	return create(c)
+}
+
+func create(c Conf) (r reconcile.Result, err error) {
+	client := c.Reconcile.GetClient()
+
+	if c.OwnerReference {
+		err = controllerutil.SetControllerReference(c.Instance, c.Object, c.Reconcile.GetScheme())
+		if err != nil {
+			return reconcile.Result{}, errors.Wrapf(err, "failed to set owner reference on the object")
+		}
+	}
+
+	err = client.Create(context.TODO(), c.Object)
+	if err != nil {
+		return reconcile.Result{}, errors.Wrap(err, "failed to create the object in cluster")
+	}
+
+	if c.AfterCreateFunc != nil {
+		r, err = c.AfterCreateFunc(c.Instance, c.Reconcile)
+		if err != nil {
+			return r, errors.Wrap(err, "failed to run AfterCreate hook")
+		}
+	}
+
+	return reconcile.Result{}, nil
+}
+
+// Update is a generic update function for any Kubernetes Object. This
+// can be used to update the objects using MaybeUpdateFunc defined in
+// Conf. This is a lower-level function which is supposed to be used
+// by other Objects. This can also be used to implement custom update
+// logic on any Kubernetes Object. It fetches the Object with same
+// metadata as the specified object from the cluster and update them
+// using MaybeUpdateFunc.
+func Update(c Conf) (reconcile.Result, error) {
+	return update(c)
+}
+
+func update(c Conf) (r reconcile.Result, err error) {
+	client := c.Reconcile.GetClient()
+
+	existingObject, ok := reflect.New(reflect.TypeOf(c.Object)).Interface().(interfaces.Object)
+	if !ok {
+		return reconcile.Result{}, errors.New("failed to create new instance of the object type")
+	}
+
+	err = client.Get(context.TODO(), types.NamespacedName{}, existingObject)
+	if err != nil {
+		return reconcile.Result{}, errors.Wrap(err, "failed to get the existing object from cluster")
+	}
+
+	requireUpdate, err := c.MaybeUpdateFunc(existingObject, c.Object)
+	if err != nil {
+		return reconcile.Result{}, errors.Wrap(err, "failed to update the object")
+	}
+
+	if requireUpdate {
+		err = client.Update(context.TODO(), existingObject)
+		if err != nil {
+			return reconcile.Result{}, errors.Wrap(err, "failed to update the object in cluster")
+		}
+	}
+
+	if c.AfterUpdateFunc != nil {
+		r, err = c.AfterUpdateFunc(c.Instance, c.Reconcile)
+		if err != nil {
+			return r, errors.Wrap(err, "failed to run AfterUpdate hook")
+		}
+	}
+
+	return reconcile.Result{}, nil
+}
+
+// CreateOrUpdate is the combination of Create and Update. It can be
+// used to create any Kubernetes Object. It catches the
+// "IsAlreadyExists" error and tries to update the object.
+func CreateOrUpdate(c Conf) (r reconcile.Result, err error) {
+	r, err = create(c)
+	if err != nil && !kerrors.IsAlreadyExists(err) {
+		return r, err
+	}
+
+	return update(c)
+}
+
+// Delete is a generic delete function for any Kubernetes Object. This
+// function can be used to delete any Kubernetes Object defined in
+// Conf. This is a lower-level function which is supposed to be used
+// by other Kubernetes Objects. This can also be used to delete any
+// Custom Objects (or unsupported Objects).
+func Delete(c Conf) (r reconcile.Result, err error) {
+	return delete(c)
+}
+
+func delete(c Conf) (r reconcile.Result, err error) {
+	client := c.Reconcile.GetClient()
+
+	err = client.Delete(context.TODO(), c.Object)
+	if !kerrors.IsNotFound(err) {
+		return reconcile.Result{}, errors.Wrap(err, "failed to delete the object in cluster")
+	}
+
+	if c.AfterDeleteFunc != nil {
+		r, err = c.AfterDeleteFunc(c.Instance, c.Reconcile)
+		if err != nil {
+			return r, errors.Wrap(err, "failed to run AfterDelete hook")
+		}
+	}
+
+	return reconcile.Result{}, nil
+}
